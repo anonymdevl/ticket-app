3daf9f453650ff66eac92f7a6ce419f5c6b3ca9b
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2023-11-22 02:51:18 +0000

    Added Dynamic Fetching of Tickets in Tickets Page from Database

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,7 +1,36 @@
-const StatusDisplay = () => {
+const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color = 'bg-slate-400';
+    switch (status.toString().toLowerCase()) {
+      case 'done':
+        color = 'bg-green-200';
+        return color;
+      case 'working':
+        color = 'bg-yellow-200';
+        return color;
+      case 'not started':
+        color = 'bg-red-200';
+        return color;
+      case 'pending':
+        color = 'bg-indigo-200';
+        return color;
+      case 'review':
+        color = 'bg-pink-300';
+        return color;
+      case 'open':
+        color = 'bg-red-400';
+        return color;
+    }
+    return color;
+  };
+
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200 ">
-      done
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
+      {status}
     </span>
   );
 };
--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,6 +1,7 @@
 import Ticket from '@/app/(models)/Ticket';
 import { NextResponse } from 'next/server';
 
+// To display dummy tickets for starters and for testing
 export async function POST(req) {
   try {
     const body = await req.json();
@@ -8,6 +9,16 @@ export async function POST(req) {
     await Ticket.create(ticketData);
 
     return NextResponse.json({ message: 'Ticket Created' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500 });
+  }
+}
+
+// To fetch all our tickets from our database and display them on our ticket display screen
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: 'Error', error }, { status: 500 });
   }
